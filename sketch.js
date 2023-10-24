e25665640322e9994d7c944263acc4697ab15505
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2023-10-24 12:38:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,9 @@ var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
 }
 
 function setup() {
